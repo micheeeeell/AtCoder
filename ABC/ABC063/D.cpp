2c9fee416ba39f91c2680bc7637bd2a666169f99
Shina Takada <stkd1001@gmail.com>
2019-12-02 15:07:47 +0900

    2019/12/02

---
+++ ABC/ABC063/D.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<cstdio>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<map>
+using namespace std;
+#define rep(i,x) for(int i = 0; i < (int)(x); i++)
+#define pb push_back
+#define mp make_pair
+#define debug(x) cerr << #x << ": " << (x) << "\n";
+#define all(x) (x).begin(), (x).end()
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> P;
+typedef pair<ll,ll> Pll;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<ll>>> vvvl;
+const ll MOD = 1e9+7;
+const int n_max = 1e5+10;
+
+void print() {
+    cout << endl;
+}
+
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+    cout << head;
+    if (sizeof...(tail) != 0) cout << ' ';
+    print(forward<Tail>(tail)...);
+}
+
+template <class T>
+void print(vector<T> &vec) {
+    for (auto& a : vec) {
+        cout << a;
+        if (&a != &vec.back()) cout << ' ';
+    }
+    cout << endl;
+}
+
+template <class T>
+void print(vector<vector<T>> &df) {
+    for (auto& vec : df) {
+        print(vec);
+    }
+}
+
+template<typename T>
+void print(T &arr){
+    ll sz = sizeof(arr)/sizeof(arr[0]);
+    for(int i = 0;i < sz-1;i++){cout << arr[i] << ' ';}
+    cout << arr[sz-1] << endl;
+}
+
+
+int main(){
+    ll n,a,b; cin >> n >> a >> b;
+    vector<ll> h(n);
+    rep(i,n) cin >> h[i];
+    a -= b;
+    auto check = [&](ll x) -> bool {
+        vector<ll> temp = h;
+        rep(i,n) temp[i] -= x*b;
+        rep(i,n){
+            if(0 < temp[i])x -= (temp[i] + a - 1)/a;
+        }
+        return 0 <= x;
+    };
+    ll ok = 1e9 + 1000;
+    ll ng = 0;
+    while(abs(ok - ng) > 1){
+        ll x = (ok+ng)/2;
+        if(check(x))ok = x;
+        else ng = x;
+        // print(ok,ng,x);
+        // print(check(x));
+    }
+
+    cout << ok << endl;
+}
