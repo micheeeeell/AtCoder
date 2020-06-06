81059befcfc83136864796d6558d5a4437dc335a
micheeeeell <52061125+micheeeeell@users.noreply.github.com>
2020-06-06 14:46:50 +0900

    Delete cptest_atcoder.sh

--- ShellScripts/cptest_atcoder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-problemname=$1
-oj login -u micheeeeell -p 10michell01 "https://atcoder.jp/"
-oj login --check "https://atcoder.jp/"
-oj dl "https://${problemname:0:6}.contest.atcoder.jp/tasks/${problemname:0:8}"
-g++ -std=c++17 -ftrapv -g ./${problemname:7:8}.cpp
-oj test
-rm -f a.out
-rm -rf test 
